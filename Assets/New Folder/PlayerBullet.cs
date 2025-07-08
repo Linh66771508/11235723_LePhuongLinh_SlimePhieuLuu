4898a7f52ef840818d58975139dea7da219c4d91
Linh66771508 <163240493+Linh66771508@users.noreply.github.com>
2025-07-08 12:05:14 +0700

    First Commit

---
+++ Assets/New Folder/PlayerBullet.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 25f;
+    [SerializeField] private float timeDestroy = 0.5f;
+
+    void Start()
+    {
+        Destroy(gameObject, timeDestroy);
+    }
+
+
+
+    void Update()
+    {
+        MoveBullet();
+    }
+
+    void MoveBullet()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage();
+            }
+            Destroy(gameObject);
+        }
+    }
+}
